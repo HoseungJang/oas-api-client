cba3c63407e5c323ae4ef9c0b6b3406512d6caa1
HoseungJang <hsjang.dev@gmail.com>
2021-04-10 19:15:17 +0900

    apply operation extractor

--- src/ClientGenerator.ts
+++ src/ClientGenerator.ts
@@ -1,21 +1,39 @@
 import { OpenAPIObject } from "openapi3-ts";
+import * as _ from "lodash";
 import * as fs from "fs/promises";
 
 import { ModelsRenderer } from "./ModelsRenderer";
+import { OperationExtractor } from "./OperationExtractor";
 
 export class ClientGenerator {
-  private readonly APIDefinition: OpenAPIObject;
-  private readonly outputDir: string;
-
-  constructor(options: { APIDefinition: OpenAPIObject; outputDir: string }) {
-    this.APIDefinition = options.APIDefinition;
-    this.outputDir = options.outputDir;
-  }
+  constructor(
+    private readonly APIDefinition: OpenAPIObject,
+    private readonly outputDir: string
+  ) {}
 
   public async generate() {
-    const models = await new ModelsRenderer(
-      this.APIDefinition.components!.schemas!
-    ).render();
+    const operations = await new OperationExtractor(
+      this.APIDefinition.paths
+    ).extract();
+
+    const models = await new ModelsRenderer({
+      ...(this.APIDefinition.components?.schemas ?? {}),
+      ..._.chain(operations)
+        .map(
+          ({
+            requestSchema,
+            requestModelName,
+            responseSchema,
+            responseModelName,
+          }) => [
+            [requestModelName, requestSchema],
+            [responseModelName, responseSchema],
+          ]
+        )
+        .flatMap((pairs) => pairs)
+        .fromPairs()
+        .value(),
+    }).render();
 
     await fs.writeFile(`${this.outputDir}/models.ts`, models);
   }
