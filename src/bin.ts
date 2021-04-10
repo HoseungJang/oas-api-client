f953eb61123701516af4e0a0191cdcd363bded94
HoseungJang <hsjang.dev@gmail.com>
2021-04-11 02:09:49 +0900

    remove Program class

--- src/bin.ts
+++ src/bin.ts
@@ -1,67 +1,59 @@
 #!/usr/bin/env node
-import { Command, OptionValues } from "commander";
+import { Command } from "commander";
 import got from "got";
 import { OpenAPIObject } from "openapi3-ts";
 import * as fs from "fs";
 
 import { ClientGenerator } from "./ClientGenerator";
 
-class Program {
-  private readonly options: OptionValues;
+function deleteDirectoryRecursive(path: string) {
+  fs.readdirSync(path).forEach((file) => {
+    const currentPath = `${path}/${file}`;
 
-  constructor() {
-    const program = new Command();
+    if (fs.lstatSync(currentPath).isDirectory()) {
+      deleteDirectoryRecursive(currentPath);
+    } else {
+      fs.unlinkSync(currentPath);
+    }
+  });
+  fs.rmdirSync(path);
+}
 
-    program
-      .option(
-        "-u, --url <url>",
-        "HTTP URL which provide OpenAPI specification (must be JSON)"
-      )
-      .option(
-        "-f, --file <path>",
-        "absolute or relative path of OpenAPI specification (must be JSON)"
-      )
-      .option("-o, --outputDir <path>", "output path", "./oasis");
+const program = new Command();
 
-    program.parse();
-    this.options = program.opts();
-  }
-
-  public async run() {
-    const { url, file, outputDir } = this.options;
+program
+  .command("generate")
+  .description("generate OAS API client")
+  .option(
+    "-u, --url <url>",
+    "HTTP URL which provide OpenAPI specification (must be JSON)"
+  )
+  .option(
+    "-f, --file <path>",
+    "absolute or relative path of OpenAPI specification (must be JSON)"
+  )
+  .option("-o, --outputDir <path>", "output path", "./oasis")
+  .action(async (options) => {
+    const { url, file, outputDir } = options;
 
     let APIDefinition: OpenAPIObject;
     if (url) {
-      APIDefinition = await got(this.options.url).json();
+      APIDefinition = await got(url).json();
     } else if (file) {
-      if (!/.json$/.test(this.options.file)) {
+      if (!/.json$/.test(file)) {
         throw new Error("unsupported file format (must be JSON)");
       }
-      APIDefinition = JSON.parse(fs.readFileSync(this.options.file).toString());
+      APIDefinition = JSON.parse(fs.readFileSync(file).toString());
     } else {
       throw new Error("--url or --file option must be provided");
     }
 
     if (fs.existsSync(outputDir)) {
-      this.deleteDirectoryRecursive(outputDir);
+      deleteDirectoryRecursive(outputDir);
     }
     fs.mkdirSync(outputDir);
 
     await new ClientGenerator(APIDefinition, outputDir).generate();
-  }
+  });
 
-  private deleteDirectoryRecursive(path: string) {
-    fs.readdirSync(path).forEach((file) => {
-      const currentPath = `${path}/${file}`;
-
-      if (fs.lstatSync(currentPath).isDirectory()) {
-        this.deleteDirectoryRecursive(currentPath);
-      } else {
-        fs.unlinkSync(currentPath);
-      }
-    });
-    fs.rmdirSync(path);
-  }
-}
-
-new Program().run();
+program.parse();
