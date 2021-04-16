36f532f349d69cade8b95b2f5057606b6ffd4806
HoseungJang <hsjang.dev@gmail.com>
2021-04-16 12:49:30 +0900

    make formatOptions optional

--- src/renderers/BaseRenderer.ts
+++ src/renderers/BaseRenderer.ts
@@ -3,7 +3,7 @@ import * as prettier from "prettier";
 export class BaseRenderer {
   private readonly formatOptions: prettier.Options;
 
-  constructor(formatOptions: prettier.Options) {
+  constructor(formatOptions?: prettier.Options) {
     this.formatOptions = {
       semi: true,
       parser: "babel-ts",
