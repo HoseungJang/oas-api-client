a93c37816abdae6397b8becfbcfbcde7156161bc
HoseungJang <hsjang.dev@gmail.com>
2021-04-16 12:46:03 +0900

    add base renderer

---
+++ src/renderer/BaseRenderer.ts
@@ -0,0 +1,17 @@
+import * as prettier from "prettier";
+
+export class BaseRenderer {
+  private readonly formatOptions: prettier.Options;
+
+  constructor(formatOptions: prettier.Options) {
+    this.formatOptions = {
+      semi: true,
+      parser: "babel-ts",
+      ...formatOptions,
+    };
+  }
+
+  protected format(code: string) {
+    return prettier.format(code);
+  }
+}
